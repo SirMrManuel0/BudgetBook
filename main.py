0299db1beb3783a8edb2e4370963a7cc2ae008f4
SirMrManuel0 <139504269+SirMrManuel0@users.noreply.github.com>
2025-07-13 16:55:06 +0200

    add port and ip to server

--- main.py
+++ main.py
@@ -10,6 +10,19 @@ parser.add_argument(
     required=True
 )
 
+parser.add_argument(
+    "--ip",
+    default="127.0.0.1",
+    required=False
+)
+
+parser.add_argument(
+    "--port",
+    type=int,
+    default=8080,
+    required=False
+)
+
 args = parser.parse_args()
 
 if keyring.get_password("BudgetBook", "system_key") is None:
